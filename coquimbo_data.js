350a4d624d43084aeaa125bb7b6710816212ab2e
ismaelvasquezl <ismaelvasquezl@gmail.com>
2026-08-21 17:06:49 -0400

    Add files via upload

---
+++ coquimbo_data.js
@@ -0,0 +1,540 @@
+const COQ={"team": {"gf": 23, "ga": 21, "gpk": 22, "pk": 1, "pkatt": 2, "cy": 50, "cr": 8, "mp": 17, "ageAvg": 30.5}, "opp": {"gf": 21, "ast": 13, "cy": 55, "cr": 5, "pkatt": 4}, "players": [{"n": "Diego Sánchez Carvajal", "nat": "CHI", "pos": "GK", "edad": 39, "mp": 14, "st": 14, "min": 1259, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 1, "cr": 1, "p90g": 0.0, "p90a": 0.0}, {"n": "Francisco Salinas", "nat": "CHI", "pos": "DF", "edad": 26, "mp": 14, "st": 14, "min": 1220, "gls": 0, "ast": 1, "ga": 1, "pk": 0, "cy": 6, "cr": 1, "p90g": 0.0, "p90a": 0.07}, {"n": "Benjamín Gazzolo", "nat": "CHI", "pos": "DF", "edad": 29, "mp": 14, "st": 14, "min": 1212, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 5, "cr": 1, "p90g": 0.0, "p90a": 0.0}, {"n": "Alejandro Camargo", "nat": "ARG", "pos": "MF", "edad": 37, "mp": 14, "st": 14, "min": 1094, "gls": 3, "ast": 0, "ga": 3, "pk": 0, "cy": 2, "cr": 0, "p90g": 0.25, "p90a": 0.0}, {"n": "Manuel Fernández", "nat": "URU", "pos": "DF", "edad": 37, "mp": 14, "st": 13, "min": 1188, "gls": 1, "ast": 0, "ga": 1, "pk": 0, "cy": 7, "cr": 2, "p90g": 0.08, "p90a": 0.0}, {"n": "Juan Cornejo", "nat": "CHI", "pos": "DF", "edad": 36, "mp": 13, "st": 13, "min": 1122, "gls": 0, "ast": 6, "ga": 6, "pk": 0, "cy": 1, "cr": 1, "p90g": 0.0, "p90a": 0.48}, {"n": "Sebastián Galani", "nat": "CHI", "pos": "MF", "edad": 29, "mp": 14, "st": 13, "min": 1108, "gls": 1, "ast": 0, "ga": 1, "pk": 0, "cy": 5, "cr": 0, "p90g": 0.08, "p90a": 0.0}, {"n": "Nicolas Johansen", "nat": "ARG", "pos": "FW", "edad": 27, "mp": 16, "st": 12, "min": 964, "gls": 6, "ast": 0, "ga": 6, "pk": 1, "cy": 1, "cr": 0, "p90g": 0.56, "p90a": 0.0}, {"n": "Guido Vadalá", "nat": "ARG", "pos": "MF", "edad": 29, "mp": 14, "st": 12, "min": 901, "gls": 3, "ast": 0, "ga": 3, "pk": 0, "cy": 2, "cr": 0, "p90g": 0.3, "p90a": 0.0}, {"n": "Luis Riveros", "nat": "PAR", "pos": "MF", "edad": 28, "mp": 15, "st": 8, "min": 737, "gls": 1, "ast": 3, "ga": 4, "pk": 0, "cy": 2, "cr": 1, "p90g": 0.12, "p90a": 0.37}, {"n": "Cristián Zavala", "nat": "CHI", "pos": "MF", "edad": 27, "mp": 12, "st": 7, "min": 671, "gls": 2, "ast": 3, "ga": 5, "pk": 0, "cy": 3, "cr": 0, "p90g": 0.27, "p90a": 0.4}, {"n": "Martín Mundaca", "nat": "CHI", "pos": "MF", "edad": 19, "mp": 11, "st": 7, "min": 463, "gls": 1, "ast": 0, "ga": 1, "pk": 0, "cy": 1, "cr": 0, "p90g": 0.19, "p90a": 0.0}, {"n": "Benjamín Chandía", "nat": "CHI", "pos": "MF,FW", "edad": 23, "mp": 15, "st": 6, "min": 692, "gls": 0, "ast": 2, "ga": 2, "pk": 0, "cy": 0, "cr": 0, "p90g": 0.0, "p90a": 0.26}, {"n": "Elvis Hernández", "nat": "ARG", "pos": "DF", "edad": 26, "mp": 7, "st": 6, "min": 535, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 1, "cr": 1, "p90g": 0.0, "p90a": 0.0}, {"n": "Alejandro Azócar", "nat": "CHI", "pos": "MF", "edad": 25, "mp": 12, "st": 6, "min": 514, "gls": 2, "ast": 1, "ga": 3, "pk": 0, "cy": 1, "cr": 0, "p90g": 0.35, "p90a": 0.18}, {"n": "Lucas Pratto", "nat": "ARG", "pos": "FW", "edad": 38, "mp": 14, "st": 5, "min": 465, "gls": 1, "ast": 0, "ga": 1, "pk": 0, "cy": 0, "cr": 0, "p90g": 0.19, "p90a": 0.0}, {"n": "Salvador Cordero", "nat": "CHI", "pos": "MF", "edad": 30, "mp": 11, "st": 4, "min": 508, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 2, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Dylan Glaby", "nat": "ARG", "pos": "MF", "edad": 30, "mp": 12, "st": 4, "min": 436, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 3, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Sebastian Cabrera", "nat": "CHI", "pos": "DF", "edad": 28, "mp": 7, "st": 4, "min": 410, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 2, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Pablo Rodríguez", "nat": "CHI", "pos": "MF", "edad": 19, "mp": 8, "st": 3, "min": 340, "gls": 2, "ast": 0, "ga": 2, "pk": 0, "cy": 3, "cr": 0, "p90g": 0.53, "p90a": 0.0}, {"n": "Dylan Escobar", "nat": "CHI", "pos": "DF", "edad": 25, "mp": 5, "st": 3, "min": 324, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 1, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Gonzalo Flores", "nat": "CHI", "pos": "GK", "edad": 26, "mp": 3, "st": 3, "min": 270, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 1, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Lukas Soza", "nat": "CHI", "pos": "DF", "edad": 28, "mp": 1, "st": 1, "min": 90, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 0, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Dixon Pereira", "nat": "nan", "pos": "MF", "edad": null, "mp": 1, "st": 1, "min": 44, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 0, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Rodrigo Holgado", "nat": "MAS", "pos": "FW", "edad": 31, "mp": 3, "st": 0, "min": 85, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 0, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Facundo Pons", "nat": "ARG", "pos": "FW", "edad": 30, "mp": 1, "st": 0, "min": 21, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 0, "cr": 0, "p90g": 0.0, "p90a": 0.0}, {"n": "Vicente Villegas", "nat": "CHI", "pos": "GK", "edad": 16, "mp": 1, "st": 0, "min": 1, "gls": 0, "ast": 0, "ga": 0, "pk": 0, "cy": 0, "cr": 0, "p90g": 0.0, "p90a": 0.0}]};
+const MATCHES=[{"date": "01-31", "comp": "LIG", "ven": "A", "opp": "U Concepción", "res": "L", "gf": 0, "ga": 1, "poss": 50.0, "shf": 8, "sof": 1, "sha": 13, "soa": 4, "cy": 4, "cr": 1, "crs": 14, "intc": 6, "tklw": 10, "form": "4-2-3-1"}, {"date": "02-07", "comp": "LIG", "ven": "H", "opp": "Palestino", "res": "W", "gf": 3, "ga": 1, "poss": 35.0, "shf": 14, "sof": 5, "sha": 13, "soa": 6, "cy": 2, "cr": 1, "crs": 14, "intc": 11, "tklw": 9, "form": "4-2-3-1"}, {"date": "02-14", "comp": "LIG", "ven": "A", "opp": "La Serena", "res": "W", "gf": 1, "ga": 0, "poss": 50.0, "shf": 13, "sof": 4, "sha": 4, "soa": 0, "cy": 2, "cr": 0, "crs": 19, "intc": 5, "tklw": 10, "form": "4-2-3-1"}, {"date": "02-21", "comp": "LIG", "ven": "A", "opp": "U. Católica", "res": "L", "gf": 1, "ga": 3, "poss": 41.0, "shf": 9, "sof": 4, "sha": 15, "soa": 7, "cy": 4, "cr": 1, "crs": 17, "intc": 11, "tklw": 6, "form": "4-2-3-1"}, {"date": "02-28", "comp": "LIG", "ven": "H", "opp": "D. Concepción", "res": "L", "gf": 0, "ga": 1, "poss": 66.0, "shf": 17, "sof": 3, "sha": 8, "soa": 3, "cy": 3, "cr": 1, "crs": 36, "intc": 4, "tklw": 12, "form": "4-2-3-1"}, {"date": "03-07", "comp": "LIG", "ven": "A", "opp": "Huachipato", "res": "W", "gf": 3, "ga": 1, "poss": 43.0, "shf": 16, "sof": 6, "sha": 12, "soa": 2, "cy": 4, "cr": 0, "crs": 26, "intc": 10, "tklw": 3, "form": "4-2-3-1"}, {"date": "03-14", "comp": "LIG", "ven": "H", "opp": "Univ. de Chile", "res": "L", "gf": 0, "ga": 1, "poss": 51.0, "shf": 19, "sof": 2, "sha": 8, "soa": 3, "cy": 2, "cr": 0, "crs": 36, "intc": 6, "tklw": 14, "form": "4-2-3-1"}, {"date": "04-03", "comp": "LIG", "ven": "H", "opp": "Cobresal", "res": "W", "gf": 3, "ga": 2, "poss": 59.0, "shf": 23, "sof": 11, "sha": 12, "soa": 4, "cy": 4, "cr": 0, "crs": 37, "intc": 11, "tklw": 20, "form": "4-2-3-1"}, {"date": "04-08", "comp": "LIB", "ven": "H", "opp": "uy Nacional", "res": "D", "gf": 1, "ga": 1, "poss": 68.0, "shf": 16, "sof": 8, "sha": 7, "soa": 2, "cy": 2, "cr": 0, "crs": 34, "intc": 14, "tklw": 12, "form": "4-2-3-1"}, {"date": "04-14", "comp": "LIB", "ven": "A", "opp": "pe Universitario", "res": "W", "gf": 2, "ga": 0, "poss": 32.0, "shf": 5, "sof": 2, "sha": 8, "soa": 4, "cy": 1, "cr": 0, "crs": 12, "intc": 8, "tklw": 4, "form": "4-2-3-1"}, {"date": "04-19", "comp": "LIG", "ven": "H", "opp": "Ñublense", "res": "D", "gf": 1, "ga": 1, "poss": 42.0, "shf": 10, "sof": 3, "sha": 15, "soa": 7, "cy": 3, "cr": 1, "crs": 20, "intc": 18, "tklw": 13, "form": "4-2-3-1"}, {"date": "04-24", "comp": "LIG", "ven": "A", "opp": "Unión La Calera", "res": "W", "gf": 2, "ga": 1, "poss": 42.0, "shf": 10, "sof": 6, "sha": 12, "soa": 4, "cy": 5, "cr": 0, "crs": 18, "intc": 13, "tklw": 12, "form": "4-4-2"}, {"date": "04-28", "comp": "LIB", "ven": "A", "opp": "co Deportes Tolima", "res": "L", "gf": 0, "ga": 3, "poss": 32.0, "shf": 9, "sof": 5, "sha": 14, "soa": 5, "cy": 3, "cr": 0, "crs": 19, "intc": 16, "tklw": 5, "form": "4-2-3-1"}, {"date": "05-03", "comp": "LIG", "ven": "A", "opp": "Colo-Colo", "res": "L", "gf": 1, "ga": 3, "poss": 39.0, "shf": 7, "sof": 3, "sha": 16, "soa": 9, "cy": 3, "cr": 0, "crs": 20, "intc": 9, "tklw": 10, "form": "4-2-3-1"}, {"date": "05-07", "comp": "LIB", "ven": "H", "opp": "pe Universitario", "res": "W", "gf": 2, "ga": 1, "poss": 53.0, "shf": 10, "sof": 4, "sha": 9, "soa": 2, "cy": 1, "cr": 0, "crs": 28, "intc": 16, "tklw": 5, "form": "4-2-3-1"}, {"date": "05-15", "comp": "LIG", "ven": "H", "opp": "Audax Italiano", "res": "W", "gf": 3, "ga": 0, "poss": 51.0, "shf": 14, "sof": 8, "sha": 5, "soa": 0, "cy": 1, "cr": 0, "crs": 23, "intc": 12, "tklw": 17, "form": "4-4-2"}, {"date": "05-19", "comp": "LIB", "ven": "H", "opp": "co Deportes Tolima", "res": "W", "gf": 3, "ga": 0, "poss": 33.0, "shf": 14, "sof": 10, "sha": 8, "soa": 2, "cy": 3, "cr": 0, "crs": 19, "intc": 16, "tklw": 7, "form": "4-2-3-1"}, {"date": "05-22", "comp": "LIG", "ven": "A", "opp": "Everton", "res": "D", "gf": 1, "ga": 1, "poss": 48.0, "shf": 13, "sof": 4, "sha": 9, "soa": 3, "cy": 3, "cr": 0, "crs": 22, "intc": 13, "tklw": 13, "form": "4-1-4-1"}, {"date": "05-26", "comp": "LIB", "ven": "A", "opp": "uy Nacional", "res": "L", "gf": 0, "ga": 1, "poss": 67.0, "shf": 8, "sof": 3, "sha": 8, "soa": 3, "cy": 4, "cr": 0, "crs": 33, "intc": 7, "tklw": 17, "form": "4-2-3-1"}, {"date": "05-31", "comp": "LIG", "ven": "A", "opp": "CD Limache", "res": "W", "gf": 3, "ga": 2, "poss": 62.0, "shf": 16, "sof": 4, "sha": 13, "soa": 5, "cy": 4, "cr": 1, "crs": 24, "intc": 10, "tklw": 8, "form": "4-2-3-1"}, {"date": "06-13", "comp": "LIG", "ven": "H", "opp": "O'Higgins", "res": "D", "gf": 0, "ga": 0, "poss": 59.0, "shf": 17, "sof": 5, "sha": 6, "soa": 2, "cy": 2, "cr": 0, "crs": 35, "intc": 9, "tklw": 6, "form": "4-2-3-1"}, {"date": "08-01", "comp": "LIG", "ven": "A", "opp": "Palestino", "res": "L", "gf": 1, "ga": 2, "poss": 51.0, "shf": 13, "sof": 4, "sha": 8, "soa": 3, "cy": 2, "cr": 1, "crs": 16, "intc": 13, "tklw": 10, "form": "4-2-3-1"}, {"date": "08-08", "comp": "LIG", "ven": "H", "opp": "La Serena", "res": "D", "gf": 1, "ga": 1, "poss": 60.0, "shf": 13, "sof": 5, "sha": 6, "soa": 1, "cy": 2, "cr": 1, "crs": 47, "intc": 8, "tklw": 14, "form": "4-2-3-1"}, {"date": "08-12", "comp": "LIB", "ven": "A", "opp": "ar Platense", "res": "D", "gf": 1, "ga": 1, "poss": 48.0, "shf": 11, "sof": 1, "sha": 16, "soa": 8, "cy": 2, "cr": 0, "crs": 28, "intc": 14, "tklw": 16, "form": "4-2-3-1"}, {"date": "08-19", "comp": "LIB", "ven": "H", "opp": "ar Platense", "res": "D", "gf": 0, "ga": 0, "poss": 40.0, "shf": 10, "sof": 3, "sha": 15, "soa": 2, "cy": 3, "cr": 0, "crs": 11, "intc": 15, "tklw": 8, "form": "4-2-3-1"}];
+
+const PLAYERS=[
+ {
+  "player": "Benjamin Gazzolo",
+  "roles": [
+   "CB",
+   "DFC"
+  ],
+  "is_gk": false,
+  "minutes_by_season": [
+   {
+    "s": "2020",
+    "min": 36
+   },
+   {
+    "s": "2021",
+    "min": 1512
+   },
+   {
+    "s": "2022",
+    "min": 1939
+   },
+   {
+    "s": "2023",
+    "min": 2183
+   },
+   {
+    "s": "2024",
+    "min": 3196
+   },
+   {
+    "s": "2025",
+    "min": 1458
+   },
+   {
+    "s": "2026",
+    "min": 1842
+   }
+  ],
+  "seasons": 7,
+  "clubs": 2,
+  "career_min": 12166,
+  "career_mp": 154,
+  "career_gls": 4,
+  "career_ast": 1,
+  "career_crdy": 36,
+  "career_crdr": 4,
+  "cards_y_per90": 0.27,
+  "red_per_season": 0.57,
+  "y2026": {
+   "mp": 21,
+   "min": 1842,
+   "age": "28",
+   "gls": 0,
+   "ast": 0,
+   "crdy": 8,
+   "crdr": 1
+  }
+ },
+ {
+  "player": "Diego Sanchez",
+  "roles": [
+   "GK"
+  ],
+  "is_gk": true,
+  "minutes_by_season": [
+   {
+    "s": "2017",
+    "min": 1254
+   },
+   {
+    "s": "2018",
+    "min": 1980
+   },
+   {
+    "s": "2019",
+    "min": 2640
+   },
+   {
+    "s": "2020",
+    "min": 2790
+   },
+   {
+    "s": "2021",
+    "min": 2115
+   },
+   {
+    "s": "2022",
+    "min": 2190
+   },
+   {
+    "s": "2023",
+    "min": 2610
+   },
+   {
+    "s": "2024",
+    "min": 2970
+   },
+   {
+    "s": "2025",
+    "min": 2610
+   },
+   {
+    "s": "2026",
+    "min": 1900
+   }
+  ],
+  "seasons": 14,
+  "clubs": 3,
+  "career_min": 30169,
+  "career_mp": 335,
+  "career_saves": 714,
+  "career_ga": 313,
+  "career_save_pct": 69.5,
+  "career_cs": 84,
+  "y2026": {
+   "mp": 21,
+   "min": 1900,
+   "age": "38",
+   "saves": 48,
+   "ga": 23,
+   "save_pct": 67.6,
+   "cs": 6
+  }
+ },
+ {
+  "player": "Elvis Hernandez",
+  "roles": [
+   "CB"
+  ],
+  "is_gk": false,
+  "minutes_by_season": [
+   {
+    "s": "2024",
+    "min": 1389
+   },
+   {
+    "s": "2025",
+    "min": 676
+   },
+   {
+    "s": "2026",
+    "min": 745
+   }
+  ],
+  "seasons": 3,
+  "clubs": 1,
+  "career_min": 2810,
+  "career_mp": 52,
+  "career_gls": 1,
+  "career_ast": 0,
+  "career_crdy": 9,
+  "career_crdr": 1,
+  "cards_y_per90": 0.29,
+  "red_per_season": 0.33,
+  "y2026": {
+   "mp": 9,
+   "min": 745,
+   "age": "26",
+   "gls": 0,
+   "ast": 0,
+   "crdy": 1,
+   "crdr": 1
+  }
+ },
+ {
+  "player": "Francisco Salinas",
+  "roles": [
+   "RB",
+   "DFD"
+  ],
+  "is_gk": false,
+  "minutes_by_season": [
+   {
+    "s": "2023",
+    "min": 1767
+   },
+   {
+    "s": "2024",
+    "min": 163
+   },
+   {
+    "s": "2025",
+    "min": 2602
+   },
+   {
+    "s": "2026",
+    "min": 1970
+   }
+  ],
+  "seasons": 4,
+  "clubs": 2,
+  "career_min": 6502,
+  "career_mp": 80,
+  "career_gls": 8,
+  "career_ast": 9,
+  "career_crdy": 16,
+  "career_crdr": 1,
+  "cards_y_per90": 0.22,
+  "red_per_season": 0.25,
+  "y2026": {
+   "mp": 22,
+   "min": 1970,
+   "age": "26",
+   "gls": 0,
+   "ast": 2,
+   "crdy": 6,
+   "crdr": 1
+  }
+ },
+ {
+  "player": "Juan Cornejo",
+  "roles": [
+   "LB",
+   "DFI"
+  ],
+  "is_gk": false,
+  "minutes_by_season": [
+   {
+    "s": "2017-2018",
+    "min": 2187
+   },
+   {
+    "s": "2018-2019",
+    "min": 943
+   },
+   {
+    "s": "2019",
+    "min": 1903
+   },
+   {
+    "s": "2020",
+    "min": 1128
+   },
+   {
+    "s": "2021",
+    "min": 1063
+   },
+   {
+    "s": "2022",
+    "min": 1897
+   },
+   {
+    "s": "2023",
+    "min": 2609
+   },
+   {
+    "s": "2024",
+    "min": 1592
+   },
+   {
+    "s": "2025",
+    "min": 2289
+   },
+   {
+    "s": "2026",
+    "min": 1872
+   }
+  ],
+  "seasons": 13,
+  "clubs": 5,
+  "career_min": 24171,
+  "career_mp": 306,
+  "career_gls": 15,
+  "career_ast": 32,
+  "career_crdy": 65,
+  "career_crdr": 1,
+  "cards_y_per90": 0.24,
+  "red_per_season": 0.08,
+  "y2026": {
+   "mp": 21,
+   "min": 1872,
+   "age": "35",
+   "gls": 0,
+   "ast": 7,
+   "crdy": 2,
+   "crdr": 1
+  }
+ },
+ {
+  "player": "Manuel Fernandez",
+  "roles": [
+   "CB",
+   "DFC"
+  ],
+  "is_gk": false,
+  "minutes_by_season": [
+   {
+    "s": "2017",
+    "min": 1147
+   },
+   {
+    "s": "2018",
+    "min": 1752
+   },
+   {
+    "s": "2019",
+    "min": 1521
+   },
+   {
+    "s": "2020",
+    "min": 2526
+   },
+   {
+    "s": "2021",
+    "min": 705
+   },
+   {
+    "s": "2022",
+    "min": 2233
+   },
+   {
+    "s": "2023",
+    "min": 1129
+   },
+   {
+    "s": "2024",
+    "min": 2306
+   },
+   {
+    "s": "2025",
+    "min": 2486
+   },
+   {
+    "s": "2026",
+    "min": 1780
+   }
+  ],
+  "seasons": 10,
+  "clubs": 3,
+  "career_min": 17585,
+  "career_mp": 215,
+  "career_gls": 12,
+  "career_ast": 2,
+  "career_crdy": 73,
+  "career_crdr": 14,
+  "cards_y_per90": 0.37,
+  "red_per_season": 1.4,
+  "y2026": {
+   "mp": 21,
+   "min": 1780,
+   "age": "37",
+   "gls": 3,
+   "ast": 0,
+   "crdy": 10,
+   "crdr": 2
+  }
+ },
+ {
+  "player": "Salvador Cordero",
+  "roles": [
+   "CB",
+   "DFI",
+   "MC"
+  ],
+  "is_gk": false,
+  "minutes_by_season": [
+   {
+    "s": "2015-2016",
+    "min": 2
+   },
+   {
+    "s": "2016-2017",
+    "min": 32
+   },
+   {
+    "s": "2018",
+    "min": 1610
+   },
+   {
+    "s": "2019",
+    "min": 535
+   },
+   {
+    "s": "2020",
+    "min": 2120
+   },
+   {
+    "s": "2021",
+    "min": 2601
+   },
+   {
+    "s": "2022",
+    "min": 2809
+   },
+   {
+    "s": "2025",
+    "min": 709
+   },
+   {
+    "s": "2026",
+    "min": 918
+   }
+  ],
+  "seasons": 9,
+  "clubs": 2,
+  "career_min": 11336,
+  "career_mp": 166,
+  "career_gls": 3,
+  "career_ast": 5,
+  "career_crdy": 27,
+  "career_crdr": 0,
+  "cards_y_per90": 0.21,
+  "red_per_season": 0.0,
+  "y2026": {
+   "mp": 18,
+   "min": 918,
+   "age": "29",
+   "gls": 0,
+   "ast": 0,
+   "crdy": 5,
+   "crdr": 0
+  }
+ },
+ {
+  "player": "Sebastian CAbrera",
+  "roles": [
+   "DFI",
+   "LI",
+   "EI"
+  ],
+  "is_gk": false,
+  "minutes_by_season": [
+   {
+    "s": "2019",
+    "min": 1468
+   },
+   {
+    "s": "2020",
+    "min": 594
+   },
+   {
+    "s": "2021",
+    "min": 568
+   },
+   {
+    "s": "2022",
+    "min": 12
+   },
+   {
+    "s": "2023",
+    "min": 987
+   },
+   {
+    "s": "2024",
+    "min": 2165
+   },
+   {
+    "s": "2025",
+    "min": 671
+   },
+   {
+    "s": "2026",
+    "min": 410
+   }
+  ],
+  "seasons": 8,
+  "clubs": 3,
+  "career_min": 6875,
+  "career_mp": 116,
+  "career_gls": 5,
+  "career_ast": 8,
+  "career_crdy": 21,
+  "career_crdr": 1,
+  "cards_y_per90": 0.27,
+  "red_per_season": 0.12,
+  "y2026": {
+   "mp": 7,
+   "min": 410,
+   "age": "27",
+   "gls": 0,
+   "ast": 0,
+   "crdy": 2,
+   "crdr": 0
+  }
+ },
+ {
+  "player": "Sebastian Galani",
+  "roles": [
+   "MCD",
+   "MC"
+  ],
+  "is_gk": false,
+  "minutes_by_season": [
+   {
+    "s": "2019",
+    "min": 2085
+   },
+   {
+    "s": "2020",
+    "min": 1101
+   },
+   {
+    "s": "2021",
+    "min": 1697
+   },
+   {
+    "s": "2022",
+    "min": 1594
+   },
+   {
+    "s": "2023",
+    "min": 2334
+   },
+   {
+    "s": "2024",
+    "min": 1985
+   },
+   {
+    "s": "2025",
+    "min": 1991
+   },
+   {
+    "s": "2026",
+    "min": 1768
+   }
+  ],
+  "seasons": 8,
+  "clubs": 3,
+  "career_min": 14555,
+  "career_mp": 193,
+  "career_gls": 2,
+  "career_ast": 3,
+  "career_crdy": 43,
+  "career_crdr": 0,
+  "cards_y_per90": 0.27,
+  "red_per_season": 0.0,
+  "y2026": {
+   "mp": 21,
+   "min": 1768,
+   "age": "28",
+   "gls": 1,
+   "ast": 0,
+   "crdy": 5,
+   "crdr": 0
+  }
+ }
+];
